e7787366f583cabfed1fb9c0984aa8745a59e47e
SRIRAM YERRA <132144497+sriram-yerra@users.noreply.github.com>
2026-01-28 07:21:31 +0000

    .

--- Project_ANPR/backend/database.py
+++ Project_ANPR/backend/database.py
@@ -10,6 +10,7 @@ engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 class Detections(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     filename: str
+    filepath: str
     timestamp: datetime = Field(default_factory=datetime.utcnow)
     inference_time_ms: float
     num_detections: int
--- Project_ANPR/backend/main.py
+++ Project_ANPR/backend/main.py
@@ -14,8 +14,14 @@ app = FastAPI(title="AI Vision API")
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 MODEL_PATH = os.path.join(BASE_DIR, "../weights/combined_best.pt")
-SAVE_DIR = os.path.join(BASE_DIR, "storage/images")
-os.makedirs(SAVE_DIR, exist_ok=True)
+IMG_SAVE_DIR = os.path.join(BASE_DIR, "storage/images")
+VID_SAVE_DIR = os.path.join(BASE_DIR, "storage/videos")
+
+if not IMG_SAVE_DIR:
+    os.makedirs(IMG_SAVE_DIR, exist_ok=True)
+
+if not VID_SAVE_DIR:
+    os.makedirs(VID_SAVE_DIR, exist_ok=True)
 
 model = YOLO(MODEL_PATH)
 
@@ -43,28 +49,39 @@ def detect_image(image_bytes):
 
 def save_image(image, file_name):
     filename = f"{file_name}_result.jpg"
-    path = os.path.join(SAVE_DIR, filename)
-    cv2.imwrite(path, image)
-    return filename   # return only filename
+    filepath = os.path.join(IMG_SAVE_DIR, filename)
+    cv2.imwrite(filepath, image)
+    return filename, filepath   # return only filename
+
+IMG_EXT = [".jpg", ".jpeg", ".png", ".gif", ".webp", ".avif", ".svg"]
 
 @app.post("/detect-image")
 async def detect(
-    custom_file_name: Optional[str] = Query(None), 
     file: UploadFile = File(...), 
+    custom_file_name: Optional[str] = Query(None), 
     session: SessionDep = None
-):
+    ):
     image_bytes = await file.read()
 
-    image_name = file.filename
+    name, ext = os.path.splitext(file.filename)
+    ext = ext.lower()
+
+    if ext not in IMG_EXT:
+        raise HTTPException(status_code=400, detail="Only image files allowed")
+
     annotated, meta = detect_image(image_bytes)
+    image_name = file.filename
     
     if custom_file_name:
-        object_name = save_image(annotated, custom_file_name)
+        object_name, file_path = save_image(annotated, custom_file_name)
     else:
-        object_name = save_image(annotated, image_name)
+        object_name, file_path = save_image(annotated, image_name)
+
+    total_detections = meta["num_detections"]
 
     record = Detections(
         filename=object_name,
+        filepath=file_path,
         inference_time_ms=meta["inference_time_ms"],
         num_detections=meta["num_detections"],
         classes_detected=meta["classes_detected"],
@@ -76,27 +93,16 @@ async def detect(
 
     return JSONResponse({
         "message": "Detections done",
-        "download_url": f"/download/{object_name}"
+        "download_url": f"{file_path}",
+        "Total Detections": f"{total_detections}",
     })
-
-'''
-For the below endpoint, fetcg the downloaded image through file id...!
-'''
-@app.get("/download/{filename}")
-def download_file(filename: str):
-    file_path = os.path.join(SAVE_DIR, filename)
-
-    if not os.path.exists(file_path):
-        raise HTTPException(status_code=404, detail="File not found")
-
-    return FileResponse(file_path, media_type="image/jpeg", filename=filename)
 
 @app.get("/detections")
 def get_detections(
     session: SessionDep, 
     file_name: Optional[int] = None, 
     file_id: Optional[str] = None
-):
+    ):
     if file_id is not None:
         record = session.get(Detections, file_id)
         if not record:
@@ -105,10 +111,71 @@ def get_detections(
 
     if file_name is not None:
         query = select(Detections).where(Detections.filename == file_name)
-        records = session.exec(
-            query
-        ).all()
+        records = session.exec(query).all()
         return records
 
     records = session.exec(select(Detections)).all()
     return records
+
+@app.get("/download/{filename}")
+def download_file(
+    session: SessionDep,
+    file_name: Optional[str] = None, 
+    file_id: Optional[int] = None, 
+    ):
+    if file_name is not None:
+        query = select(Detections).where(Detections.filename == file_name)
+        record = session.exec(query).first()
+
+    elif file_id is not None:
+        record = session.get(Detections, file_id)
+
+    else:
+        raise HTTPException(400, "Provide file_id or file_name")
+
+    path = record.file_path
+
+    if not os.path.exists(file_path):
+        raise HTTPException(status_code=404, detail="File not found")
+
+    return FileResponse(path, media_type="image/jpeg", filename=record.filename)
+
+@app.delete("/detections/all")
+def delete_all_detections(session: SessionDep):
+    records = session.exec(select(Detections)).all()
+
+    for rec in records:
+        if os.path.exists(rec.filepath):
+            os.remove(rec.filepath)   # delete image file too
+        session.delete(rec)
+
+    session.commit()
+    return {"message": "All detections cleared"}
+
+@app.delete("/detections/id/{file_id}")
+def delete_by_id(
+    session: SessionDep,
+    file_name: Optional[str] = None, 
+    file_id: Optional[int] = None, 
+    ):
+    if file_name is not None:
+        query = select(Detections).where(Detections.filename == file_name)
+        record = session.exec(query).first()
+
+    elif file_id is not None:
+        record = session.get(Detections, file_id)
+
+    else:
+        raise HTTPException(400, "Provide file_id or file_name")
+
+    if not record:
+        raise HTTPException(404, "Detection not found")
+
+    if os.path.exists(record.filepath):
+        os.remove(record.filepath)
+
+    session.delete(record)
+    session.commit()
+
+    return {"message": f"Detection {file_id} deleted"}
+
